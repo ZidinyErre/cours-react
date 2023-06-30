abdeb1c5d6a96cfb94e246ccb78c3fcaeeddcb1f
Zidiny Erre <zidiny.erre.fr@gmail.com>
2023-06-30 18:57:39 +0200

    Mise en place des routes de bases.

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation/>
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, inventore aliquam repudiandae quod consequatur voluptatem deleniti distinctio voluptate doloremque harum deserunt recusandae quam commodi eaque quibusdam consectetur mollitia unde ex numquam ducimus exercitationem non beatae quas vel. Modi nostrum, ratione iusto illum tempora amet? Officia asperiores voluptas quod assumenda eligendi ipsam facere provident sunt ullam! Placeat ipsa voluptatum repellat iure! Quam autem quas beatae nihil iste. Laborum fugiat maiores, facilis fugit recusandae minima? Eaque eius nihil totam! Earum voluptates expedita voluptas dolores consequatur at obcaecati numquam quasi laborum eaque ex, aliquam vero nisi ratione! Quo ab facere error beatae libero eaque, porro nobis enim earum? Eaque sapiente culpa eos sed ipsa aut tenetur? Quo, quae. Magnam est molestiae optio rem velit, maiores sint labore explicabo, dolores totam impedit quae odio voluptatem ullam laborum, aperiam ratione deserunt maxime veritatis tempora voluptatum ex? Molestiae eaque dolor debitis provident cumque facilis officia earum facere, illum aperiam qui aliquam quos adipisci dignissimos quam reiciendis? Non earum illo vero distinctio sed, itaque vel nostrum esse laboriosam, soluta labore iusto impedit voluptatem quia nam laborum dignissimos quidem, possimus hic eligendi odit temporibus pariatur ad. Error fugiat ipsam in iste similique minus doloribus repellendus soluta odit facilis.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, inventore aliquam repudiandae quod consequatur voluptatem deleniti distinctio voluptate doloremque harum deserunt recusandae quam commodi eaque quibusdam consectetur mollitia unde ex numquam ducimus exercitationem non beatae quas vel. Modi nostrum, ratione iusto illum tempora amet? Officia asperiores voluptas quod assumenda eligendi ipsam facere provident sunt ullam! Placeat ipsa voluptatum repellat iure! Quam autem quas beatae nihil iste. Laborum fugiat maiores, facilis fugit recusandae minima? Eaque eius nihil totam! Earum voluptates expedita voluptas dolores consequatur at obcaecati numquam quasi laborum eaque ex, aliquam vero nisi ratione! Quo ab facere error beatae libero eaque, porro nobis enim earum? Eaque sapiente culpa eos sed ipsa aut tenetur? Quo, quae. Magnam est molestiae optio rem velit, maiores sint labore explicabo, dolores totam impedit quae odio voluptatem ullam laborum, aperiam ratione deserunt maxime veritatis tempora voluptatum ex? Molestiae eaque dolor debitis provident cumque facilis officia earum facere, illum aperiam qui aliquam quos adipisci dignissimos quam reiciendis? Non earum illo vero distinctio sed, itaque vel nostrum esse laboriosam, soluta labore iusto impedit voluptatem quia nam laborum dignissimos quidem, possimus hic eligendi odit temporibus pariatur ad. Error fugiat ipsam in iste similique minus doloribus repellendus soluta odit facilis.</p>
+        </div>
+    );
+};
+
+export default About;
